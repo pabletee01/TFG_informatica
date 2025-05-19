3b806959aaed1eb6041dfc6d35daae9526312dd0
Your Name <you@example.com>
2025-05-19 22:03:12 +0200

    More tests implemented

--- test/test_relation_maker.py
+++ test/test_relation_maker.py
@@ -1,5 +1,6 @@
 import pytest
 import pymongo
+from pprint import pprint
 from app.classifier import classifier
 from app.formatter import formatter
 from app.classifier import classifier_habitat
@@ -95,24 +96,23 @@ def AUX_method(csv_file: str, C :float, iter: int, white_list: dict, black_list:
             
             for name in wl:
                 list_numbers.append(names.index(name))
+
+            list_relations = []
             
             for name in final_matrix:
                 if name[0] == key:
                     list_relations = name[1]
             
-            print(list_relations)
-            print(wl)
-            print(list_numbers)
-            
             list_numbers_aux = list_numbers.copy()
             
             for n in list_numbers_aux:
-                if list_relations[n][2] == 1:
-                    index = list_numbers.index(n)
-                    list_numbers.remove(n)
-                    print(wl, index)
-                    elem = wl[index]
-                    wl.remove(elem)
+                if len(list_relations) > 0:
+                    if list_relations[n][2] == 1:
+                        index = list_numbers.index(n)
+                        list_numbers.remove(n)
+                        print(wl, index)
+                        elem = wl[index]
+                        wl.remove(elem)
                     
             print(list_numbers)
             
@@ -151,7 +151,7 @@ def AUX_method(csv_file: str, C :float, iter: int, white_list: dict, black_list:
     return True if len(white_list_keys) == 0 and not(black_list_flag) else False
 
 # Method used in the tests to show frequencies of the relations made by the algorithm
-def AUX_method_frequencies(csv_file: str, C :float, iter: int, white_list: dict):
+def AUX_method_frequencies(csv_file: str, C :float, iter: int, white_list: dict, file: str):
     
     # Loading habitat
     formatter(csv_file, "test_collection")
@@ -210,6 +210,8 @@ def AUX_method_frequencies(csv_file: str, C :float, iter: int, white_list: dict)
             wl = white_list[key]
             
             list_numbers = []
+
+            list_relations = []
             
             for name in wl:
                 list_numbers.append(names.index(name))
@@ -226,27 +228,50 @@ def AUX_method_frequencies(csv_file: str, C :float, iter: int, white_list: dict)
             
             # Contar aquí
             for n in list_numbers_aux:
-                if list_relations[n][2] == 1:
-                    index = list_numbers.index(n)
-                    print(wl, index)
-                    result_d[key][index][1] += 1
+                if len(list_relations) > 0:
+                    if list_relations[n][2] == 1:
+                        index = list_numbers.index(n)
+                        print(wl, index)
+                        result_d[key][index][1] += 1
                     
             #print(list_numbers)
 
                     
-    
-    print(white_list)
-    print(white_list_keys)
-    print(result_d)
+    with open(file, "w") as f:
+        pprint(result_d, f)
 
-    return 
+    return True
 
 def test_relation_maker_ok(mongo_db_non_curated, mongo_db_curated):
     white_list = {
         "Bacteria2": ["Bacteria1", "Bacteria3"],
     }
     black_list = {
-        "Bacteria1": ["Bacteria2"],
+        "Bacteria3": ["Bacteria1", "Bacteria2"]
     }
-    assert AUX_method("test_ok.csv",1,1,white_list, black_list)
+
+    assert AUX_method("test_ok.csv",1,100, white_list, black_list)
+
+# Checking if not related by category organisms create relations
+def test_relation_maker_not_related_category(mongo_db_non_curated, mongo_db_curated):
+    black_list = {
+        "Bacteria2": ["Bacteria1"],
+        "Bacteria3": ["Bacteria4"]
+    }
+    
+    assert AUX_method("test1.csv",1,100,{},black_list)
+
+# Checking if organisms living in distinct niches predate each other
+def test_relation_maker_different_niche_never_related(mongo_db_non_curated, mongo_db_curated):
+
+    white_list = {
+        "Bacteria5": ["Bacteria1", "Bacteria2"]
+    }
+
+    black_list = {
+        "Bacteria3": ["Bacteria1", "Bacteria2"],
+        "Bacteria4": ["Bacteria1", "Bacteria2"]
+    }
+
+    assert AUX_method("test2.csv",1,100,white_list,black_list)
     
